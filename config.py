6827da5fca4172a9790f82ea1aabaa1d0069be93
dkorostashov-droid <d.korostashov@gmail.com>
2025-10-09 04:23:07 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,10 @@
 # config.py
 
-BOT_TOKEN = "PASTE_YOUR_TOKEN_HERE"  # встав свій токен сюди
+BOT_TOKEN = "8328512172:AAEaOGMTWKZeIUZytbHLvaAIz1kSdA0NaVQ"  # встав свій токен сюди
 SPREADSHEET_NAME = "LCWAIKIKI_candidates"
 WORKSHEET_NAME = "work"
 HR_CHAT_ID = -1003187426680  # канал, куди летять заявки
 
 # Назва JSON-файлу для Google Sheets API
 GOOGLE_CREDENTIALS_FILE = "credentials.json"
+
